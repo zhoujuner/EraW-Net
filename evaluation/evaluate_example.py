36d0bab9ae506567cdb29d5c4fc33197454d4e7a
Zhouj <84528255+zhoujuner@users.noreply.github.com>
2026-06-30 16:03:28 +0800

    Update evaluate_example.py

--- evaluation/evaluate_example.py
+++ evaluation/evaluate_example.py
@@ -1,5 +1,5 @@
 import torch
-from metrics import Matrix_Loss, mae
+from metrics import Matrix_Loss
 
 
 if __name__ == "__main__":
@@ -9,9 +9,8 @@ if __name__ == "__main__":
 
     criterion = Matrix_Loss(kld_parm=1, cc_parm=-0.1, sim_parm=-0.2, nss_parm=-0.005)
     _, kld, cc, sim, nss = criterion(pred, gt)
-    mae_sal = mae(pred, gt)
 
-    print("MAE_sal:", mae_sal.item())
+
     print("KLD:", kld.item())
     print("CC:", cc.item())
     print("SIM:", sim.item())
